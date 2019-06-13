8b10f6dce55e52d8d652931fe52c80f99eda14bc
zhuolinho <zhuolinho@hotmail.com>
2019-06-13 15:53:00 +0800

    header and footer

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -1,10 +1,10 @@
 module.exports = {
   navTheme: 'light', // theme for nav menu
-  primaryColor: '#F5222D', // primary color of ant design
+  primaryColor: '#c81d48', // primary color of ant design
   layout: 'topmenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
   fixedHeader: false, // sticky header
   autoHideHeader: false, // auto hide header
   fixSiderbar: false, // sticky siderbar
-  title: 'Ant Design Pro',
+  title: 'VR Platform',
 };
--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,37 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import { Layout, Icon } from 'antd';
 import GlobalFooter from '@/components/GlobalFooter';
+import { connect } from 'dva';
 
 const { Footer } = Layout;
-const FooterView = () => (
-  <Footer style={{ padding: 0 }}>
-    <GlobalFooter
-      links={[
-        {
-          key: 'Pro 首页',
-          title: 'Pro 首页',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: <Icon type="github" />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
-      ]}
-      copyright={
-        <Fragment>
-          Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
-        </Fragment>
-      }
-    />
-  </Footer>
-);
+@connect(({ setting }) => ({ setting }))
+class FooterView extends PureComponent {
+  render() {
+    const {
+      setting: { primaryColor },
+    } = this.props;
+    return (
+      <Footer style={{ padding: 0, backgroundColor: primaryColor }}>
+        <GlobalFooter
+          links={[
+            {
+              key: 'Pro 首页',
+              title: 'Pro 首页',
+              href: 'https://pro.ant.design',
+              blankTarget: true,
+            },
+            {
+              key: 'github',
+              title: <Icon type="github" />,
+              href: 'https://github.com/ant-design/ant-design-pro',
+              blankTarget: true,
+            },
+            {
+              key: 'Ant Design',
+              title: 'Ant Design',
+              href: 'https://ant.design',
+              blankTarget: true,
+            },
+          ]}
+          copyright={
+            <Fragment>
+              Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
+            </Fragment>
+          }
+        />
+      </Footer>
+    );
+  }
+}
 export default FooterView;
