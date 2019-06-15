5992a0b9a6deb8c969cedb07a9e90bcf5f2c8233
zhuolinho <zhuolinho@hotmail.com>
2019-06-15 18:07:11 +0800

    video

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import mv from '@/assets/MV 720P .mp4';
+import { Card } from 'antd';
 // import classNames from 'classnames';
 // import styles from './index.less';
 //   const clsString = classNames(styles.globalFooter, className);
 
-const Home = () => <div>home</div>;
+const Home = () => (
+  <Card style={{ textAlign: 'center' }}>
+    <video src={mv} controls>
+      <track kind="captions" />
+    </video>
+  </Card>
+);
 export default Home;
