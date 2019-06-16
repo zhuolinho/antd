2605edaa5ec9bfac0e368990187e92cc5919a837
zhuolinho <zhuolinho@hotmail.com>
2019-06-16 15:03:41 +0800

    about us

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import mv from '@/assets/MV 720P .mp4';
-import { Card } from 'antd';
+import aboutImg from '@/assets/about.jpg';
+import { Card, List } from 'antd';
 import styles from './index.less';
+import ie from '@/assets/ie.png';
+import edge from '@/assets/edge.png';
+import firefox from '@/assets/firefox.png';
+import chrome from '@/assets/chrome.png';
+import safari from '@/assets/safari.png';
 // import classNames from 'classnames';
 //   const clsString = classNames(styles.globalFooter, className);
 
-const Home = () => (
+const data = [
+  { name: 'IE', src: ie },
+  { name: 'Chrome', src: chrome },
+  { name: 'Edge', src: edge },
+  { name: 'Firefox', src: firefox },
+  { name: 'Safari', src: safari },
+];
+
+const About = () => (
   <Card>
     <Card className={styles.context} title="关于我们">
-      <video src={mv} controls>
-        <track kind="captions" />
-      </video>
+      <img src={aboutImg} alt="about us" style={{ width: '100%' }} />
       这里是关于我们的介绍
     </Card>
-    <Card className={styles.context} title="网络要求" />
-
+    <Card className={styles.context} title="网络要求">
+      <List
+        grid={{ column: 5 }}
+        dataSource={data}
+        renderItem={item => (
+          <List.Item style={{ textAlign: 'center' }}>
+            <img src={item.src} alt={item.name} style={{ height: '100px' }} />
+            <p>{item.name}</p>
+          </List.Item>
+        )}
+      />
+    </Card>
     <Card className={styles.context} title="联系我们">
       <p>电话：12300000000</p>
       <p>邮箱：123@sjtu.edu.com</p>
     </Card>
   </Card>
 );
-export default Home;
+export default About;
