d8e236e0b8f01ba878e060e04bfab18bc0b22ce0
zhuolinho <zhuolinho@hotmail.com>
2019-06-18 20:35:07 +0800

    login

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -1,35 +1,8 @@
-import React, { Fragment } from 'react';
-import { formatMessage } from 'umi/locale';
+import React from 'react';
 import Link from 'umi/link';
-import { Icon } from 'antd';
-import GlobalFooter from '@/components/GlobalFooter';
 import SelectLang from '@/components/SelectLang';
 import styles from './UserLayout.less';
-import logo from '../assets/logo.svg';
-
-const links = [
-  {
-    key: 'help',
-    title: formatMessage({ id: 'layout.user.link.help' }),
-    href: '',
-  },
-  {
-    key: 'privacy',
-    title: formatMessage({ id: 'layout.user.link.privacy' }),
-    href: '',
-  },
-  {
-    key: 'terms',
-    title: formatMessage({ id: 'layout.user.link.terms' }),
-    href: '',
-  },
-];
-
-const copyright = (
-  <Fragment>
-    Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
-  </Fragment>
-);
+import logo from '../assets/logo.png';
 
 class UserLayout extends React.PureComponent {
   // @TODO title
@@ -56,14 +29,12 @@ class UserLayout extends React.PureComponent {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+            <div className={styles.desc}>产品设计虚拟仿真与认知评价教学实验平台</div>
           </div>
           {children}
         </div>
-        <GlobalFooter links={links} copyright={copyright} />
       </div>
     );
   }
