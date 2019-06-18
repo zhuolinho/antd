1c452e27b0b7eac948833e0d345a6b354c6f4ffc
zhuolinho <zhuolinho@hotmail.com>
2019-06-18 16:03:51 +0800

    add experiment

--- src/pages/Experiment/index.js
+++ src/pages/Experiment/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Button } from 'antd';
-import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import { Card, List } from 'antd';
+// import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import styles from './index.less';
+import one from '@/assets/1111.png';
+import two from '@/assets/2222.png';
+import three from '@/assets/3333.png';
+import four from '@/assets/4444.png';
+
+const data = [
+  { name: '步态分析实验', src: one },
+  { name: '眼动仪实验', src: two },
+  { name: '多导仪实验', src: three },
+  { name: '面部识别实验', src: four },
+];
 
 const About = () => (
-  <PageHeaderWrapper>
-    <Button
-      onClick={() => {
-        window.location = '/vr1';
-      }}
-    >
-      Click Me
-    </Button>
-  </PageHeaderWrapper>
+  <Card>
+    <Card className={styles.context} title="VR实验">
+      <List
+        grid={{ column: 4 }}
+        dataSource={data}
+        renderItem={item => (
+          <List.Item>
+            <img src={item.src} alt={item.name} style={{ height: '150px' }} />
+            <p>{item.name}</p>
+          </List.Item>
+        )}
+      />
+    </Card>
+  </Card>
 );
 export default About;
