ff37491e87e08b8caf67b465e2533a98bcf7889a
zhuolinho <zhuolinho@hotmail.com>
2019-07-25 15:30:18 +0800

    login redirect

--- src/models/login.js
+++ src/models/login.js
@@ -1,8 +1,7 @@
-import { routerRedux } from 'dva/router';
+import request from '@/utils/request';
 // import { stringify } from 'qs';
 import { userLogin, getFakeCaptcha } from '@/services/api';
 import { setAuthority } from '@/utils/authority';
-import { getPageQuery } from '@/utils/utils';
 import { reloadAuthorized } from '@/utils/Authorized';
 
 export default {
@@ -22,22 +21,9 @@ export default {
           payload: { status: 'ok', currentAuthority: response.role, type: 'account' },
         });
         reloadAuthorized();
-        const urlParams = new URL(window.location.href);
-        const params = getPageQuery();
-        let { redirect } = params;
-        if (redirect) {
-          const redirectUrlParams = new URL(redirect);
-          if (redirectUrlParams.origin === urlParams.origin) {
-            redirect = redirect.substr(urlParams.origin.length);
-            if (redirect.match(/^\/.*#/)) {
-              redirect = redirect.substr(redirect.indexOf('#') + 1);
-            }
-          } else {
-            window.location.href = redirect;
-            return;
-          }
-        }
-        yield put(routerRedux.replace(redirect || '/'));
+        request('/api/vr').then(res => {
+          window.location = res.path;
+        });
       }
     },
 
