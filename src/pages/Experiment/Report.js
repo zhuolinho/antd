0001ff0710b0f3ab0673278631f3048a4ff899c6
zhuolinho <zhuolinho@hotmail.com>
2019-08-01 01:24:28 +0800

    report and score

--- src/pages/Experiment/Report.js
+++ src/pages/Experiment/Report.js
@@ -1,4 +1,25 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Card, Row, Col } from 'antd';
 
-const Report = () => <Fragment />;
+import button1 from '@/assets/shit1.png';
+import button2 from '@/assets/shit2.png';
+import button3 from '@/assets/shit3.png';
+
+const Report = () => (
+  <Card style={{ padding: '0 150px' }}>
+    <Row>
+      <Col span={4}>
+        <img style={{ width: '100%' }} alt="" src={button1} />
+      </Col>
+    </Row>
+    <Row style={{ margin: '40px 0' }}>
+      <Col span={12}>
+        <img style={{ width: '80%' }} alt="" src={button2} />
+      </Col>
+      <Col span={12}>
+        <img style={{ width: '80%' }} alt="" src={button3} />
+      </Col>
+    </Row>
+  </Card>
+);
 export default Report;
